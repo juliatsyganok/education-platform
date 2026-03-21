273bc1e02bbf65b79b919d04a04c2d36880cd783
Julia Tsyganok <julia.tsyganok.9@yandex.ru>
2026-03-22 01:33:12 +0300

    исправлены ошибки в моделях

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,31 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Cource(models.Model):
+class Course(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    data = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
         return self.title
     
 class Lesson(models.Model):
-    cource = models.ForeignKey(Cource, on_delete=models.CASCADE, related_name='lessons')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
     name = models.CharField(max_length=200)
     content = models.TextField()
-    num = models.IntegerField()
+    order = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
     
 class Enrollment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    cource = models.ForeignKey(Cource, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     date_enrolled = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together = ('user', 'course')
+
     def __str__(self):
-        return f"{self.user.username} enrolled in {self.cource.title}"
+        return f"{self.user.username} enrolled in {self.course.title}"
     
 
 class UserProfile(models.Model):
